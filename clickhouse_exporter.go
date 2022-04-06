29b75414189cbe7e11e16f0d62ae857efbaaea78
Sun4Git <8815299+young-0@users.noreply.github.com>
2022-04-06 09:17:53 +0800

    Update clickhouse_exporter.go

--- clickhouse_exporter.go
+++ clickhouse_exporter.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"net/url"
 	"os"
+	"time"
+
+	// "os"
 
 	"github.com/ClickHouse/clickhouse_exporter/exporter"
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,11 +19,12 @@ import (
 var (
 	listeningAddress    = flag.String("telemetry.address", ":9116", "Address on which to expose metrics.")
 	metricsEndpoint     = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
-	clickhouseScrapeURI = flag.String("scrape_uri", "http://localhost:8123/", "URI to clickhouse http endpoint")
-	clickhouseOnly      = flag.Bool("clickhouse_only", false, "Expose only Clickhouse metrics, not metrics from the exporter itself")
+	clickhouseScrapeURI = flag.String("scrape_uri", "http://clickhouse-domain-com:8123/", "URI to clickhouse http endpoint")
+	clickhouseOnly      = flag.Bool("clickhouse_only", true, "Expose only Clickhouse metrics, not metrics from the exporter itself")
 	insecure            = flag.Bool("insecure", true, "Ignore server certificate if using https")
 	user                = os.Getenv("CLICKHOUSE_USER")
 	password            = os.Getenv("CLICKHOUSE_PASSWORD")
+	cacheTime		= flag.Int("cache_time",300, "cache time")
 )
 
 func main() {
@@ -52,6 +57,18 @@ func main() {
 			</body>
 			</html>`))
 	})
+
+	fmt.Println("init iniTickerFunc, cacheTime ", *cacheTime)
+	var duration int = *cacheTime
+    ticker1 := time.NewTicker(time.Duration(duration) * time.Second)
+    defer ticker1.Stop()
+    go func(t *time.Ticker) {
+        for {
+            <-t.C
+			exporter.CacheResult = nil
+            fmt.Println("Ticker: clear cache", time.Now().Format("2006-01-02 15:04:05"))
+        }
+    }(ticker1)
 
 	log.Fatal(http.ListenAndServe(*listeningAddress, nil))
 }
